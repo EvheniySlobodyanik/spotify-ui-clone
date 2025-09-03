550b00dd69b8b750e27064befff8b4f102758919
EvheniySlobodyanik <exiletopchik@gmail.com>
2025-09-03 18:24:26 +0300

    preparation for mainWrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import { Header } from "./components/Header/Header";
+import { Main } from "./components/Main/Main";
+import { Sidebar } from "./components/Sidebar/Sidebar";
+
+import styles from "./App.module.scss";
 
 function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.mainWrapper}>
+        <Sidebar />
+        <Main />
+      </div>
     </div>
   );
 }
