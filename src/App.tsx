88f76044a75c5aa3bd86b6e974728e1c5fecf1e4
EvheniySlobodyanik <exiletopchik@gmail.com>
2025-09-05 13:31:18 +0300

    import preps for footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Header } from "./components/Header/Header";
-import { Main } from "./components/Main/Main";
 import { Sidebar } from "./components/Sidebar/Sidebar";
+import { Main } from "./components/Main/Main";
+import { Footer } from "./components/Footer/Footer";
 
 import styles from "./App.module.scss";
 
@@ -13,6 +14,8 @@ function App() {
         <Sidebar />
         <Main />
       </div>
+
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,24 @@
+import ConnectDeviceIcon from "../../assets/footer/connect-device.svg?react";
+import FullscreenIcon from "../../assets/footer/fullscreen.svg?react";
+import MixIcon from "../../assets/footer/mix.svg?react";
+import PauseIcon from "../../assets/footer/pause.svg?react";
+import QueueIcon from "../../assets/footer/queue.svg?react";
+import RepeatIcon from "../../assets/footer/repeat.svg?react";
+import SkipBackIcon from "../../assets/footer/skip-back.svg?react";
+import SkipForwardIcon from "../../assets/footer/skip-forward.svg?react";
+import SoundIcon from "../../assets/footer/sound.svg?react";
+import SquareIcon from "../../assets/footer/square-icon.svg?react";
+
+import styles from "./Footer.module.scss";
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div></div>
+
+      <div></div>
+
+      <div></div>
+    </footer>
+  );
+}
